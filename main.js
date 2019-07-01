50b6e778fc400773bc006dbcd1205dbe26975336
flavio-bernasconi <info@flaviobernasconi.it>
2019-07-01 14:27:24 +0200

    .

---
+++ main.js
@@ -0,0 +1,15 @@
+var nome, cognome, colore, cost, password;
+
+nome = prompt("Digita il tuo nome");
+console.log(nome);
+
+cognome = prompt('Digita il tuo cognome');
+console.log(cognome);
+
+colore = prompt('Digita il tuo colore preferito');
+console.log(colore);
+
+cost = 19;
+
+password = nome + cognome + colore + cost;
+console.log(password);
